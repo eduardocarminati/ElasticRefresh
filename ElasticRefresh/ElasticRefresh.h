a98adb4e5fbfca305e27277154a19289fa1f5b1c
Eduardo Carminati <c1251619@bb.com.br>
2017-10-24 13:53:06 -0200

    Initial Commit

---
+++ ElasticRefresh/ElasticRefresh.h
@@ -0,0 +1,19 @@
+//
+//  ElasticRefresh.h
+//  ElasticRefresh
+//
+//  Created by Eduardo Carminati on 24/10/17.
+//  Copyright © 2017 Eduardo Carminati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElasticRefresh.
+FOUNDATION_EXPORT double ElasticRefreshVersionNumber;
+
+//! Project version string for ElasticRefresh.
+FOUNDATION_EXPORT const unsigned char ElasticRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElasticRefresh/PublicHeader.h>
+
+
